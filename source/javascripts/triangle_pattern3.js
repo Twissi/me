163e2245641d099365e2eaa14c91cf0bcc277102
twissi <ich@kristin-albert.de>
2014-08-12 08:50:09 +0200

    Initial commit

---
+++ source/javascripts/triangle_pattern3.js
@@ -0,0 +1,106 @@
+var trianglePattern = (function () {
+
+  var createDrawArea, createTriangle, createSquare, options, draw, triangles = [];
+
+  options = {
+      resizeFactor: 10,
+      triangleCount: 100,
+      colorArray: ["#fc2488", "#00bb9c", "#dc0043", "#ffba00", "#f68784", "#c1006a", "#d7d3bd"],
+  };
+
+  createDrawArea = function(div, width, height){
+    draw = SVG(div).size(width, height);
+  }
+
+  createTriangle = function(coords){
+    var color, random, polygon;
+
+    random = Math.floor(0 + Math.random() * options.colorArray.length);
+    color = options.colorArray[random];
+    polygon = draw.polygon(coords).fill(color);
+    polygon.on('mouseover', mouseoverEvent);
+    triangles.push(polygon);
+  };
+
+  createSquare = function(row, line){
+    var random, x, y, z, squareA, squareB, squareC, squareD;
+    squareA = { x:row*options.triangleSize, y:line*options.triangleSize };
+    squareB = { x:row*options.triangleSize, y:line*options.triangleSize+options.triangleSize };
+    squareC = { x:row*options.triangleSize+options.triangleSize, y:line*options.triangleSize };
+    squareD = { x:row*options.triangleSize+options.triangleSize, y:line*options.triangleSize+options.triangleSize };
+
+    random = Math.random()<.5;
+    if(random){
+      createTriangle(squareA.x + "," + squareA.y + " " + squareB.x + "," + squareB.y + " " + squareC.x + "," + squareC.y);
+      createTriangle(squareC.x + "," + squareC.y + " " + squareB.x + "," + squareB.y + " " + squareD.x + "," + squareD.y);
+    } else {
+      createTriangle(squareA.x + "," + squareA.y + " " + squareC.x + "," + squareC.y + " " + squareD.x + "," + squareD.y);
+      createTriangle(squareA.x + "," + squareA.y + " " + squareD.x + "," + squareD.y + " " + squareB.x + "," + squareB.y);
+    }
+  };
+
+  mouseoverEvent = function() {
+    // if(this.attr('fill-opacity') == 1){
+    //   this.fill({ color: this.attr('fill'), opacity: 0 });
+    // } else {
+    //   this.fill({ color: this.attr('fill'), opacity: 1 });
+    // }
+    this.fill({ color: this.attr('fill'), opacity: 0 });
+  }
+
+  return {
+    generate: function(div){
+      var l, r, maxColumns, maxRows, drawArea;
+
+      drawArea = document.getElementById(div);
+      maxRows = 4;
+
+      maxColumns = Math.ceil(drawArea.offsetWidth/90);
+      if( maxColumns < 3 ){
+        maxColumns = 3;
+      }
+
+      options.triangleSize = drawArea.offsetWidth / maxColumns;
+
+      createDrawArea(div, drawArea.offsetWidth, options.triangleSize*maxRows);
+      document.getElementById('secretMessage').style.height = options.triangleSize*maxRows + 'px';
+
+      for (r = 0; r < maxRows; r++) {
+        for (l = 0; l < maxColumns; l++) {
+          createSquare(l,r);
+        }
+      }
+    },
+
+    update: function(div){
+      //much faster than using innerHTML
+      var drawArea = document.getElementById(div);
+      while (drawArea.firstChild) {
+          drawArea.removeChild(drawArea.firstChild);
+      }
+      trianglePattern.generate(div);
+    },
+
+    randomShake: function(){
+      var triangle;
+
+      triangle = triangles[Math.floor(0 + Math.random() * triangles.length-1)];
+
+console.log("hh");
+      triangle.animate(1000,'<>',10000).fill({ color: triangle.attr('fill'), opacity: 0 });
+      console.log("hh");
+      triangle.animate(1000,'<>',10000).fill({ color: triangle.attr('fill'), opacity: 1 });
+    }
+  };
+})();
+
+window.onload = function(){
+  trianglePattern.generate("triangleBackground");
+  setTimeout(trianglePattern.randomShake(), 2000);
+};
+
+window.onresize = function() {
+  trianglePattern.update("triangleBackground");
+};
+
+

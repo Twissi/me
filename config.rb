ef37194aea82600f9a57d22bf41f8d64d3b2ac9f
twissi <ich@kristin-albert.de>
2014-08-13 00:02:19 +0200

    Group articles by category

--- config.rb
+++ config.rb
@@ -8,6 +8,12 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   blog.prefix = "posts"
 
+  blog.custom_collections = {
+    category: {
+      link: '/categories/{category}.html',
+      template: '/category.html'
+    }
+  }
   # blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
   # blog.sources = "{year}-{month}-{day}-{title}.html"
@@ -20,13 +26,13 @@ activate :blog do |blog|
   # blog.day_link = "{year}/{month}/{day}.html"
   # blog.default_extension = ".markdown"
 
-  blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
+  # blog.tag_template = "tag.html"
+  # blog.calendar_template = "calendar.html"
 
   # Enable pagination
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/{num}"
+  blog.paginate = true
+  blog.per_page = 3
+  blog.page_link = "page/{num}"
 end
 
 page "/feed.xml", layout: false

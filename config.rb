3dd19045fc767affeadce9c46461ffd90d8bec45
twissi <ich@kristin-albert.de>
2014-08-14 00:29:52 +0200

    Change build config to use http prefix

--- config.rb
+++ config.rb
@@ -96,8 +96,8 @@ configure :build do
   activate :asset_hash
 
   # Use relative URLs
-  activate :relative_assets
+  # activate :relative_assets
 
   # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  set :http_prefix, "/me-online/"
 end

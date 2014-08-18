f75969ec83d26b035af9bd91f0c4e7e646bceb1e
Kristin Albert <kalbert@cellular.de>
2014-08-19 00:47:53 +0200

    Put writer on new line

--- config.rb
+++ config.rb
@@ -75,13 +75,13 @@ activate :livereload
  helpers do
    def get_quote
      quotes = [
-      "Don't be afraid to give up the good for the great. - John D. Rockefeller",
-      "Become friends with people who arent't your age. Hang out with people whose first language isn't the same as yours. Get to know someone who doesn't come from your social class. This is how you see the world. This is how you grow. - Unknown",
-      "The greatest prison people live in, is the fear of what other people think. - Unknown",
-      "The shortes way to yourself is around the world. - Richard Hoffman",
-      "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time. - Thomas A. Edison",
-      "Start where you are. Use what you have. Do what you can. - Arthur Ashe",
-      "Things do not happen. Things are made to happen - John F. Kennedy",
+      "Don't be afraid to give up the good for the great.<br/>John D. Rockefeller",
+      "Become friends with people who arent't your age. Hang out with people whose first language isn't the same as yours. Get to know someone who doesn't come from your social class. This is how you see the world. This is how you grow.<br/>Unknown",
+      "The greatest prison people live in, is the fear of what other people think.<br/>Unknown",
+      "The shortes way to yourself is around the world.<br/>Richard Hoffman",
+      "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.<br/>Thomas A. Edison",
+      "Start where you are. Use what you have. Do what you can.<br/>Arthur Ashe",
+      "Things do not happen. Things are made to happen.<br/>John F. Kennedy",
     ]
     quotes.sample
    end

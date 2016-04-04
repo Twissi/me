d05605e322e691fe4177fc96c31b4e9b815708f8
Kristin Fritsch <ich@kristin-fritsch.de>
2016-04-03 21:22:02 -0400

    Smaller fixes

--- source/javascripts/triangle_pattern.js
+++ source/javascripts/triangle_pattern.js
@@ -9,7 +9,7 @@ var trianglePattern = (function () {
       //colorArray: ["#8AFF27", "#1AD7FF", "#2DE879", "#E8CF18", "#FFAA29"],
       // colorArray: ["#69BCFF", "#7ED5FF", "#83A1E8", "#D7EFFF"],
       //colorArray: ["#F1396D", "#F3FFEB", "#ACC95F", "#FD6081", "#88A012"]
-      colorArray: ["#75D966", "#CCF279", "#D9B991", "#734D2D"]
+      colorArray: ["#DFF299", "#CCF279", "#D9B991", "#734D2D"]
   };
 
   createDrawArea = function(div, width, height){

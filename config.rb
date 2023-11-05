84a9f3b65b9021f805c712373f29e4661a04ce88
Kristin Fritsch <ich@kristin-fritsch.de>
2023-11-05 20:46:54 +0100

    Add sharing meta data

--- config.rb
+++ config.rb
@@ -8,6 +8,7 @@
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+page "/birthday*", :layout => "birthday"
 
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'

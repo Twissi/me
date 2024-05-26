0d65a06a34fcd8e14295b5be7bd232dc5bcbbc5a
Kristin Fritsch <ich@kristin-fritsch.de>
2024-05-26 10:50:48 +0200

    Add lego gta site

---
+++ src/content/lego-gta/images.js
@@ -0,0 +1,350 @@
+export const images231120 = [
+  {
+    path: "231120/IMG_2927.jpg",
+    description: "Roboter mit Infrarotsensor - kann Hindernisse erkennen",
+  },
+  {
+    path: "231120/IMG_2928.jpg",
+    description: "Roboter mit Infrarotsensor - kann Hindernisse erkennen",
+  },
+  {
+    path: "231120/IMG_2929.jpg",
+    description: "Roboter mit Infrarotsensor - kann Hindernisse erkennen",
+  },
+  {
+    path: "231120/IMG_2930.jpg",
+    description: "Roboter mit Infrarotsensor - kann Hindernisse erkennen",
+  },
+  {
+    path: "231120/IMG_2931.jpg",
+    description: "Roboter mit Infrarotsensor - kann Hindernisse erkennen",
+  },
+  {
+    path: "231120/IMG_2933.jpg",
+    description: "Roboter mit Infrarotsensor - kann Hindernisse erkennen",
+  },
+  {
+    path: "231120/IMG_2934.jpg",
+    description: "Roboter mit Infrarotsensor - kann Hindernisse erkennen",
+  },
+  {
+    path: "231120/IMG_2935.jpg",
+    description: "Roboter mit Infrarotsensor - kann Hindernisse erkennen",
+  },
+  {
+    path: "231120/IMG_2936.jpg",
+    description: "Roboter mit Infrarotsensor - kann Hindernisse erkennen",
+  },
+  {
+    path: "231120/IMG_2937.jpg",
+    description: "Roboter mit Infrarotsensor - kann Hindernisse erkennen",
+  },
+  {
+    path: "231120/IMG_2938.jpg",
+    description: "Roboter mit Infrarotsensor - kann Hindernisse erkennen",
+  },
+  {
+    path: "231120/IMG_2939.jpg",
+    description: "Roboter mit Infrarotsensor - kann Hindernisse erkennen",
+  },
+];
+
+export const images231127 = [
+  {
+    path: "231127/IMG_4303.jpg",
+    description: "Förderband - kann Gegestände befördern",
+  },
+  {
+    path: "231127/IMG_4304.jpg",
+    description: "Förderband - kann Gegestände befördern",
+  },
+  {
+    path: "231127/IMG_4305.jpg",
+    description: "Förderband - kann Gegestände befördern",
+  },
+  {
+    path: "231127/IMG_4306.jpg",
+    description: "Förderband - kann Gegestände befördern",
+  },
+];
+
+export const images231204 = [
+  {
+    path: "231204/IMG_3022.jpg",
+    description: "Sammler - kann Gegenstände einsammeln",
+  },
+  {
+    path: "231204/IMG_3023.jpg",
+    description: "Sammler - kann Gegenstände einsammeln",
+  },
+  {
+    path: "231204/IMG_3024.jpg",
+    description: "Sammler - kann Gegenstände einsammeln",
+  },
+  {
+    path: "231204/IMG_3025.jpg",
+    description: "Sammler - kann Gegenstände einsammeln",
+  },
+  {
+    path: "231204/IMG_3026.jpg",
+    description: "Sammler - kann Gegenstände einsammeln",
+  },
+  {
+    path: "231204/IMG_3027.jpg",
+    description: "Sammler - kann Gegenstände einsammeln",
+  },
+  {
+    path: "231204/IMG_3028.jpg",
+    description: "Sammler - kann Gegenstände einsammeln",
+  },
+  {
+    path: "231204/IMG_3029.jpg",
+    description: "Sammler - kann Gegenstände einsammeln",
+  },
+  {
+    path: "231204/IMG_3030.jpg",
+    description: "Sammler - kann Gegenstände einsammeln",
+  },
+  {
+    path: "231204/IMG_3031.jpg",
+    description: "Sammler - kann Gegenstände einsammeln",
+  },
+  {
+    path: "231204/IMG_3032.jpg",
+    description: "Sammler - kann Gegenstände einsammeln",
+  },
+  {
+    path: "231204/IMG_3033.jpg",
+    description: "Sammler - kann Gegenstände einsammeln",
+  },
+];
+
+export const images231211 = [
+  {
+    path: "231211/IMG_3103.jpg",
+    description: "Linienfolger - kann an einer Linien entlang fahren",
+  },
+  {
+    path: "231211/IMG_3104.jpg",
+    description: "Linienfolger - kann an einer Linien entlang fahren",
+  },
+  {
+    path: "231211/IMG_3106.jpg",
+    description: "Linienfolger - kann an einer Linien entlang fahren",
+  },
+];
+
+export const images231218 = [
+  { path: "231218/IMG_3202.jpg", description: "Kettenfahrzeug mit Sensoren" },
+  { path: "231218/IMG_3203.jpg", description: "Kettenfahrzeug mit Sensoren" },
+  { path: "231218/IMG_3204.jpg", description: "Kettenfahrzeug mit Sensoren" },
+  { path: "231218/IMG_3205.jpg", description: "Kettenfahrzeug mit Sensoren" },
+  { path: "231218/IMG_3206.jpg", description: "Kettenfahrzeug mit Sensoren" },
+  {
+    path: "231218/IMG_3207.jpg",
+    description: "Transportfahrzeug mit Sensoren",
+  },
+  {
+    path: "231218/IMG_3208.jpg",
+    description: "Transportfahrzeug mit Sensoren",
+  },
+  {
+    path: "231218/IMG_3209.jpg",
+    description: "Transportfahrzeug mit Sensoren",
+  },
+  {
+    path: "231218/IMG_3210.jpg",
+    description: "Transportfahrzeug mit Sensoren",
+  },
+  { path: "231218/IMG_3212.jpg", description: "Weihnachtsmann-Roboter" },
+  { path: "231218/IMG_3214.jpg", description: "Weihnachtsmann-Roboter" },
+  { path: "231218/IMG_3217.jpg", description: "Kettenfahrzeug" },
+  { path: "231218/IMG_3218.jpg", description: "Kettenfahrzeug" },
+  { path: "231218/IMG_3219.jpg", description: "Kettenfahrzeug" },
+  { path: "231218/IMG_3227.jpg", description: "Kettenfahrzeug" },
+  { path: "231218/IMG_3222.jpg", description: "Weihnachtsschlitten" },
+  { path: "231218/IMG_3224.jpg", description: "Weihnachtsschlitten" },
+  { path: "231218/IMG_3225.jpg", description: "Weihnachtsschlitten" },
+  { path: "231218/IMG_3226.jpg", description: "Weihnachtsschlitten" },
+];
+
+export const images240129 = [
+  {
+    path: "240129/IMG_3644.jpg",
+    description: "Weltraum-Expedition - MSL-Roboter in Not",
+  },
+  {
+    path: "240129/IMG_3644.jpg",
+    description: "Weltraum-Expedition - MSL-Roboter in Not",
+  },
+];
+
+export const images240226 = [
+  {
+    path: "240226/IMG_4204.jpg",
+    description: "Weltraum-Expedition - den Satelliten aussetzen",
+  },
+  {
+    path: "240226/IMG_4206.jpg",
+    description: "Weltraum-Expedition - den Satelliten aussetzen",
+  },
+  {
+    path: "240226/IMG_4208.jpg",
+    description: "Weltraum-Expedition - den Satelliten aussetzen",
+  },
+  {
+    path: "240226/IMG_4211.jpg",
+    description: "Weltraum-Expedition - den Satelliten aussetzen",
+  },
+  {
+    path: "240226/IMG_4213.jpg",
+    description: "Weltraum-Expedition - den Satelliten aussetzen",
+  },
+  {
+    path: "240226/IMG_4214.jpg",
+    description: "Weltraum-Expedition - den Satelliten aussetzen",
+  },
+];
+
+export const images240304 = [
+  {
+    path: "240304/IMG_4239.jpg",
+    description: "Weltraum-Expedition - Gesteinsproben sammeln",
+  },
+  {
+    path: "240304/IMG_4240.jpg",
+    description: "Weltraum-Expedition - Gesteinsproben sammeln",
+  },
+  {
+    path: "240304/IMG_4241.jpg",
+    description: "Weltraum-Expedition - Gesteinsproben sammeln",
+  },
+  {
+    path: "240304/IMG_4242.jpg",
+    description: "Weltraum-Expedition - Gesteinsproben sammeln",
+  },
+  {
+    path: "240304/IMG_4243.jpg",
+    description: "Weltraum-Expedition - Gesteinsproben sammeln",
+  },
+  {
+    path: "240304/IMG_4244.jpg",
+    description: "Weltraum-Expedition - Gesteinsproben sammeln",
+  },
+  {
+    path: "240304/IMG_4247.jpg",
+    description: "Weltraum-Expedition - Gesteinsproben sammeln",
+  },
+];
+
+export const images240318 = [
+  {
+    path: "240318/IMG_4303 2.jpg",
+    description: "Weltraum-Expedition - Gesteinsproben Sortiermaschine",
+  },
+  {
+    path: "240318/IMG_4304 2.jpg",
+    description: "Weltraum-Expedition - Gesteinsproben Sortiermaschine",
+  },
+  {
+    path: "240318/IMG_4305.jpg",
+    description: "Weltraum-Expedition - Gesteinsproben Sortiermaschine",
+  },
+  {
+    path: "240318/IMG_4306 2.jpg",
+    description: "Weltraum-Expedition - Gesteinsproben Sortiermaschine",
+  },
+];
+
+export const images240415 = [
+  {
+    path: "240415/IMG_5149.jpg",
+    description: "Weltraum-Expedition - den Sonnenkollektor ausklappen",
+  },
+  {
+    path: "240415/IMG_5151.jpg",
+    description: "Weltraum-Expedition - den Sonnenkollektor ausklappen",
+  },
+  {
+    path: "240415/IMG_5152.jpg",
+    description: "Weltraum-Expedition - den Sonnenkollektor ausklappen",
+  },
+  {
+    path: "240415/IMG_5153.jpg",
+    description: "Weltraum-Expedition - den Sonnenkollektor ausklappen",
+  },
+  {
+    path: "240415/IMG_5155.jpg",
+    description: "Weltraum-Expedition - den Sonnenkollektor ausklappen",
+  },
+  {
+    path: "240415/IMG_5156.jpg",
+    description: "Weltraum-Expedition - den Sonnenkollektor ausklappen",
+  },
+  {
+    path: "240415/IMG_5157.jpg",
+    description: "Weltraum-Expedition - den Sonnenkollektor ausklappen",
+  },
+];
+
+export const images240422 = [
+  {
+    path: "240422/IMG_5250.jpg",
+    description: "Weltraum-Expedition - die Rakete starten",
+  },
+  {
+    path: "240422/IMG_5252.jpg",
+    description: "Weltraum-Expedition - die Rakete starten",
+  },
+  {
+    path: "240422/IMG_5253.jpg",
+    description: "Weltraum-Expedition - die Rakete starten",
+  },
+  {
+    path: "240422/IMG_5254.jpg",
+    description: "Weltraum-Expedition - die Rakete starten",
+  },
+  {
+    path: "240422/IMG_5256.jpg",
+    description: "Weltraum-Expedition - die Rakete starten",
+  },
+  {
+    path: "240422/IMG_5258.jpg",
+    description: "Weltraum-Expedition - die Rakete starten",
+  },
+];
+
+export const images240506 = [
+  {
+    path: "240506/IMG_5345.jpeg",
+    description: "Tor mit elektronischem Torzähler",
+  },
+  { path: "240506/IMG_5346.jpeg", description: "elektronischer Torzähler" },
+  {
+    path: "240506/IMG_5347.jpeg",
+    description: "Tor mit elektronischem Torzähler",
+  },
+  {
+    path: "240506/IMG_5351.jpeg",
+    description: "Tor mit elektronischem Torzähler",
+  },
+  {
+    path: "240506/IMG_5353.jpeg",
+    description: "Tor mit elektronischem Torzähler",
+  },
+  {
+    path: "240506/IMG_5355.jpeg",
+    description: "Tor mit elektronischem Torzähler",
+  },
+  { path: "240506/IMG_5348.jpeg", description: "Fußball-Roboter" },
+  { path: "240506/IMG_5349.jpeg", description: "Fußball-Roboter" },
+  { path: "240506/IMG_5350.jpeg", description: "Fußball-Roboter" },
+  { path: "240506/IMG_5352.jpeg", description: "Fußball-Roboter" },
+  { path: "240506/IMG_5354.jpeg", description: "Fußball-Roboter" },
+  { path: "240506/IMG_5356.jpeg", description: "Fußball-Roboter" },
+  { path: "240506/IMG_5357.jpeg", description: "Fußball-Roboter" },
+  {
+    path: "240506/IMG_5361.jpeg",
+    description: "Fußball-Roboter mit Fernbedienung",
+  },
+];
--- src/content/news/2022-01-01_luise-otto-peters.md
+++ src/content/news/2022-01-01_luise-otto-peters.md
@@ -8,3 +8,5 @@ target: Schüler*innen
 2023 / 2024
 
 Wöchentlicher Lego Mindstorm Kurs im Rahmen des Ganztagsangebotes.
+
+[https://kristin-fritsch.de/lego-gta](/lego-gta)

d619d7b69d36b71883b169871644353b207fbfcd
Kristin Fritsch <ich@kristin-fritsch.de>
2025-12-04 20:58:17 +0100

    Tailwind4 migration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [mdx()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
